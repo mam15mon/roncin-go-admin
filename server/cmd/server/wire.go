286126fca048ce3f884fbc0295f7d4f7c1e8d3ec
mam15mon <mam15mon@hgj.local>
2026-08-20 11:30:28 +0800

    初始化 Kratos 与 Ant Design Pro 官方脚手架

---
+++ server/cmd/server/wire.go
@@ -0,0 +1,24 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"log/slog"
+
+	"github.com/go-kratos/kratos-layout/internal/biz"
+	"github.com/go-kratos/kratos-layout/internal/conf"
+	"github.com/go-kratos/kratos-layout/internal/data"
+	"github.com/go-kratos/kratos-layout/internal/server"
+	"github.com/go-kratos/kratos-layout/internal/service"
+
+	"github.com/go-kratos/kratos/v3"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, *slog.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
