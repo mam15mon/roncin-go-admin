c379960442ce1d4d9e1d985334d097d9124891db
mam15mon <mam15mon@hgj.local>
2026-08-23 21:03:02 +0800

    refactor(web): 优化订单表单栅格排版，实现5列/3列/2列层次分明的紧凑布局

--- web/src/pages/orders/templates/air-template.tsx
+++ web/src/pages/orders/templates/air-template.tsx
@@ -26,8 +26,9 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
       title: '基本信息',
       content: (
         <>
+          {/* 第 1 行：核心商务信息（一行 5 个） */}
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 24, lg: 12, xl: 7 }}
             name="customerId"
             label="客户单位"
             rules={[{ required: true, message: '请选择客户单位' }]}
@@ -38,7 +39,7 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
             request={async ({ keyWords }) => searchCustomers(keyWords)}
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 5 }}
             name="carrierId"
             label="承运人 (航司)"
             fieldProps={{
@@ -48,7 +49,7 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
             request={async ({ keyWords }) => searchCarriers(keyWords)}
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 4 }}
             name="bookingAgentId"
             label="订舱代理"
             fieldProps={{
@@ -58,7 +59,7 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
             request={async ({ keyWords }) => searchBookingAgents(keyWords)}
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 4 }}
             name="tradeTerm"
             label="贸易条款"
             rules={[{ required: true, message: '请选择贸易条款' }]}
@@ -66,15 +67,17 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
             placeholder="请选择贸易条款"
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 4 }}
             name="paymentTerm"
             label="付款条款"
             rules={[{ required: true, message: '请选择付款条款' }]}
             options={paymentTermOptions}
             placeholder="请选择付款条款"
           />
+
+          {/* 第 2 行：服务与分类（一行 3 个） */}
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 10 }}
             name="serviceTypeIds"
             label="服务类型"
             mode="multiple"
@@ -82,7 +85,7 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
             placeholder="请选择服务类型 (多选)"
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 10 }}
             name="cargoCategoryIds"
             label="货类"
             mode="multiple"
@@ -90,7 +93,7 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
             placeholder="请选择货类 (多选)"
           />
           <ProFormDatePicker
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 4 }}
             name="orderDate"
             label="订单日期"
             fieldProps={{ style: { width: '100%' } }}
@@ -103,8 +106,9 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
       title: '航班信息',
       content: (
         <>
+          {/* 第 1 行：航线与航班（一行 3 个，各占 8 栅格） */}
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 8 }}
             name="originLocationId"
             label="起运机场 / 地点"
             options={locationOptions}
@@ -112,7 +116,7 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
             placeholder="请选择起运机场或地点"
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 8 }}
             name="destinationLocationId"
             label="目的机场 / 地点"
             options={locationOptions}
@@ -120,25 +124,27 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
             placeholder="请选择目的机场或地点"
           />
           <ProFormText
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 8 }}
             name="vesselVoyage"
             label="航班号"
             placeholder="请输入航班号 (如 CA1234)"
           />
+
+          {/* 第 2 行：时间节点（一行 3 个，各占 8 栅格） */}
           <ProFormDatePicker
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 8 }}
             name="etd"
             label="预计起飞时间 (ETD)"
             fieldProps={{ style: { width: '100%' } }}
           />
           <ProFormDatePicker
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 8 }}
             name="eta"
             label="预计到达时间 (ETA)"
             fieldProps={{ style: { width: '100%' } }}
           />
           <ProFormDateTimePicker
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 8 }}
             name="customsCutoff"
             label="报关截关时间"
             fieldProps={{ style: { width: '100%' } }}
@@ -151,32 +157,35 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
       title: '货物信息',
       content: (
         <>
+          {/* 第 1 行：品名与特殊要求（一行 2 个，各占 12 栅格） */}
           <ProFormTextArea
-            colProps={{ span: 24 }}
+            colProps={{ xs: 24, lg: 12 }}
             name="goodsDescription"
             label="货物描述"
             placeholder="请输入货物描述"
-            fieldProps={{ maxLength: 1000, showCount: true }}
+            fieldProps={{ maxLength: 1000, showCount: true, rows: 3 }}
           />
+          <ProFormTextArea
+            colProps={{ xs: 24, lg: 12 }}
+            name="specialRequirements"
+            label="特殊要求"
+            placeholder="请输入客户或货物的特殊航空运输/装卸要求"
+            fieldProps={{ maxLength: 1000, showCount: true, rows: 3 }}
+          />
+
+          {/* 第 2 行：件数与包装（一行 2 个，各占 8 栅格） */}
           <ProFormDigit
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 8 }}
             name="totalPackages"
             label="总件数"
             min={0}
             placeholder="请输入总件数"
           />
           <ProFormText
-            colProps={{ xs: 24, sm: 12, md: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 8 }}
             name="totalPackageUnit"
             label="包装单位"
             placeholder="例如: CTNS, PLTS"
-          />
-          <ProFormTextArea
-            colProps={{ span: 24 }}
-            name="specialRequirements"
-            label="特殊要求"
-            placeholder="请输入客户或货物的特殊航空运输/装卸要求"
-            fieldProps={{ maxLength: 1000, showCount: true }}
           />
         </>
       ),
@@ -190,7 +199,7 @@ export function getAirTemplateSections(props: TemplateProps): TemplateSection[] 
           name="notes"
           label="业务备注"
           placeholder="请输入内部业务备注说明"
-          fieldProps={{ maxLength: 1000, showCount: true }}
+          fieldProps={{ maxLength: 1000, showCount: true, rows: 3 }}
         />
       ),
     },
--- web/src/pages/orders/templates/sea-template.tsx
+++ web/src/pages/orders/templates/sea-template.tsx
@@ -37,22 +37,22 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
       title: '业务信息',
       content: (
         <>
+          {/* 第 1 行：业务核心标识（一行 5 个，紧凑排布） */}
           <ProFormText
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 5 }}
             name="orderNo"
             label="订单编号"
             placeholder="保存后自动生成"
             fieldProps={{ disabled: true }}
           />
           <ProFormDatePicker
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 4 }}
             name="orderDate"
             label="订单日期"
             fieldProps={{ style: { width: '100%' } }}
           />
-          <Col span={24} style={{ height: 0 }} />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 24, lg: 12, xl: 7 }}
             name="customerId"
             label="委托单位"
             rules={[{ required: true, message: '请选择客户单位' }]}
@@ -63,52 +63,56 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
             request={async ({ keyWords }) => searchCustomers(keyWords)}
           />
           <ProFormRadio.Group
-            colProps={{ xs: 24, lg: 8, xl: 8 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 4 }}
             name="shipmentMode"
             label="集运 / 跨境"
             options={shipmentModeOptions}
           />
           <ProFormRadio.Group
-            colProps={{ xs: 24, lg: 8, xl: 10 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 4 }}
             name="shipmentType"
             label="托运类型"
             options={shipmentTypeOptions}
           />
+
+          {/* 第 2 行：服务类型与货物品类（一行 2 个，双列多选组） */}
           <ProFormCheckbox.Group
-            colProps={{ span: 24 }}
+            colProps={{ xs: 24, lg: 12 }}
             name="serviceTypeIds"
             label="服务类型"
             options={serviceTypeOptions}
           />
           <ProFormCheckbox.Group
-            colProps={{ span: 24 }}
+            colProps={{ xs: 24, lg: 12 }}
             name="cargoCategoryIds"
             label="货物品类"
             options={cargoCategoryOptions}
           />
+
+          {/* 第 3 行：商务条款与船公司（一行 5 个） */}
           <ProFormText
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 5 }}
             name="customerReferenceNo"
             label="客户业务编号"
             fieldProps={{ maxLength: 100 }}
             placeholder="请输入客户业务编号"
           />
           <ProFormText
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 4 }}
             name="contractNo"
             label="合约号"
             fieldProps={{ maxLength: 100 }}
             placeholder="请输入合约号"
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 4 }}
             name="tradeTerm"
             label="贸易条款"
             rules={[{ required: true, message: '请选择贸易条款' }]}
             options={tradeTermOptions}
             placeholder="请选择贸易条款"
           />
-          <Col xs={24} sm={12} lg={8} xl={6}>
+          <Col xs={24} sm={12} lg={6} xl={5}>
             <Form.Item label="货值">
               <Space.Compact block>
                 <Form.Item
@@ -147,14 +151,14 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
                     optionFilterProp="label"
                     options={currencyOptions}
                     placeholder="币种"
-                    style={{ width: 130 }}
+                    style={{ width: 110 }}
                   />
                 </Form.Item>
               </Space.Compact>
             </Form.Item>
           </Col>
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="carrierId"
             label="船公司"
             fieldProps={{
@@ -163,8 +167,10 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
             }}
             request={async ({ keyWords }) => searchCarriers(keyWords)}
           />
+
+          {/* 第 4 行：代理协作（一行 2 个，各占 8 栅格） */}
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 8, xl: 8 }}
             name="bookingAgentId"
             label="订舱代理"
             fieldProps={{
@@ -174,7 +180,7 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
             request={async ({ keyWords }) => searchBookingAgents(keyWords)}
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 8, xl: 8 }}
             name="foreignAgentId"
             label="国外代理"
             fieldProps={{
@@ -191,8 +197,9 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
       title: '配舱信息',
       content: (
         <>
+          {/* 第 1 行：航线 4 港口（一行 4 个，各占 6 栅格） */}
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="originLocationId"
             label="起运港"
             options={locationOptions}
@@ -200,7 +207,7 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
             placeholder="请选择起运港或地点"
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="destinationLocationId"
             label="目的港"
             options={locationOptions}
@@ -208,7 +215,7 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
             placeholder="请选择目的港或地点"
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="dischargeLocationId"
             label="卸货港"
             options={locationOptions}
@@ -216,58 +223,62 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
             placeholder="请选择卸货港"
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="transitLocationId"
             label="中转港"
             options={locationOptions}
             fieldProps={{ showSearch: true }}
             placeholder="请选择中转港"
           />
+
+          {/* 第 2 行：箱货与船期（一行 4 个） */}
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 5 }}
             name="containerOwnership"
             label="货主箱标记"
             options={containerOwnershipOptions}
             placeholder="请选择 COC / SOC"
           />
           <ProFormText
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 7 }}
             name="vesselVoyage"
             label="船名航次"
             placeholder="请输入船名航次"
           />
           <ProFormDatePicker
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="etd"
             label="ETD"
             fieldProps={{ style: { width: '100%' } }}
           />
           <ProFormDatePicker
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="eta"
             label="ETA"
             fieldProps={{ style: { width: '100%' } }}
           />
+
+          {/* 第 3 行：4 大截关时间（一行 4 个，各占 6 栅格） */}
           <ProFormDateTimePicker
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="siCutoff"
             label="SI截关时间"
             fieldProps={{ style: { width: '100%' } }}
           />
           <ProFormDateTimePicker
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="docCutoff"
             label="单证截关时间"
             fieldProps={{ style: { width: '100%' } }}
           />
           <ProFormDateTimePicker
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="customsCutoff"
             label="报关截关时间"
             fieldProps={{ style: { width: '100%' } }}
           />
           <ProFormDateTimePicker
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 6, xl: 6 }}
             name="vgmCutoff"
             label="VGM截关时间"
             fieldProps={{ style: { width: '100%' } }}
@@ -280,35 +291,38 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
       title: '提单与货物',
       content: (
         <>
+          {/* 第 1 行：品名与特殊要求（一行 2 个，各占 12 栅格） */}
           <ProFormTextArea
             colProps={{ xs: 24, lg: 12 }}
             name="goodsDescription"
             label="品名 / 货物描述"
             placeholder="请输入中英文品名或货物描述"
-            fieldProps={{ maxLength: 1000, showCount: true }}
+            fieldProps={{ maxLength: 1000, showCount: true, rows: 3 }}
           />
           <ProFormTextArea
             colProps={{ xs: 24, lg: 12 }}
             name="specialRequirements"
             label="特殊要求"
             placeholder="请输入客户或货物的特殊运输、装卸要求"
-            fieldProps={{ maxLength: 1000, showCount: true }}
+            fieldProps={{ maxLength: 1000, showCount: true, rows: 3 }}
           />
+
+          {/* 第 2 行：件数、单位与付款方式（一行 3 个，各占 8 栅格） */}
           <ProFormDigit
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 8, xl: 8 }}
             name="totalPackages"
             label="委托总件数"
             min={0}
             placeholder="请输入总件数"
           />
           <ProFormText
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 8, xl: 8 }}
             name="totalPackageUnit"
             label="件数单位"
             placeholder="例如: CTNS, PLTS"
           />
           <ProFormSelect
-            colProps={{ xs: 24, sm: 12, lg: 8, xl: 6 }}
+            colProps={{ xs: 24, sm: 12, lg: 8, xl: 8 }}
             name="paymentTerm"
             label="付款方式"
             rules={[{ required: true, message: '请选择付款方式' }]}
@@ -327,7 +341,7 @@ export function getSeaTemplateSections(props: TemplateProps): TemplateSection[] 
           name="notes"
           label="内部操作备注"
           placeholder="请输入订舱、配舱或操作过程中需要内部协作的信息"
-          fieldProps={{ maxLength: 1000, showCount: true }}
+          fieldProps={{ maxLength: 1000, showCount: true, rows: 3 }}
         />
       ),
     },
